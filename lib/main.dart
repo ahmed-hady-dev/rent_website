8d6c3a568874587dfed97bf9441ba0b9d3c260d6
ahmed-hady-org <ahmed.hady.dev@gmail.com>
2023-11-13 16:01:38 +0200

    integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,10 +8,12 @@ import 'core/dioHelper/dio_helper.dart';
 import 'core/hive_helper/hive_helper.dart';
 import 'core/router/router.dart';
 import 'core/theme/theme.dart';
+import 'firebase_options.dart';
 import 'view/auth/login/login_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   DioHelper.init();
   await EasyLocalization.ensureInitialized();
   await HiveHelper.init();
