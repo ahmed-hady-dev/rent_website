8de0c2fd2a78a3cfd1244f959baa10138a8d5981
ahmed-hady-org <ahmed.hady.dev@gmail.com>
2024-01-14 16:45:29 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:rent_website/view/ad/ad_view.dart';
+import 'package:rent_website/view/auth/login/login_view.dart';
 import 'package:sizer/sizer.dart';
 
 import 'core/dioHelper/dio_helper.dart';
@@ -36,9 +36,9 @@ class MyApp extends StatelessWidget {
         onGenerateRoute: onGenerateRoute,
         themeMode: ThemeMode.light,
         theme: lightTheme(context),
-        home: const AdView(),
+        // home: const AdView(),
         // TODO: make the home page
-        // home: const LoginView(),
+        home: const LoginView(),
         locale: context.locale,
         supportedLocales: context.supportedLocales,
         localizationsDelegates: context.localizationDelegates,
